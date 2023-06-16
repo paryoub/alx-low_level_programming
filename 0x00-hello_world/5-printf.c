c9996329c09b0b6a531408c99dd992fc930eeff9
ENNAMRI <aub.enmer@gmail.com>
2023-06-16 08:13:44 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - a program that print using printf fuction
+* main - main entry point 
 * Return: 0 (success)
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-Return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+Return (0);
 }
