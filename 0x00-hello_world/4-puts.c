02f1ae3d2e28bdaca7306ab780fd50679b158b5c
ENNAMRI <aub.enmer@gmail.com>
2023-06-16 06:40:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that print with puts function 
- * RETURN:0 (success)
- */
+*
+main - A program that print with puts function 
+* RETURN:0 (success)
+*/
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
