0cdec9f6682f1d42e3995b707f3a015e33d5aff2
ENNAMRI <aub.enmer@gmail.com>
2023-06-16 07:28:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-*main - A program that print with puts fuction
-*RETURN:Always 0 (success)
+* main - A program that print with puts fuction
+* Return: Always 0 (success)
 */
 int main(void)
 {
