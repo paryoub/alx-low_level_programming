5c5da0c392ad66868210ccd56db436e93d70fcac
ENNAMRI <aub.enmer@gmail.com>
2023-06-17 08:39:04 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,23 +6,23 @@
 
 /**
 
- * main - Entry point
+* main - Entry point
 
- *
+*
 
- * Return: Always 0 (Success)
+* Return: Always 0 (Success)
 
- */
+*/
 
 int main(void)
 
 {
 
-int n;
+int n ;
 
 srand(time(0));
 
-n = rand() - RAND_MAX / 2;
+n = rand() - RAND_MAX / 2 ;
 
 /* your code goes there */
 
