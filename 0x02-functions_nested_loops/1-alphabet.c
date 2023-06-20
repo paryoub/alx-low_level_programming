a44d6af5e5e05a1f047eaa9151c9a54a3f91d754
ENNAMRI <aub.enmer@gmail.com>
2023-06-20 08:25:57 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet use 
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 97 ; n <= 122 ; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
