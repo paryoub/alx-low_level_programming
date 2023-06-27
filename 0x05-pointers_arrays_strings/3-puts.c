c4ae50f01f37e4a3b4994debe60dbd7b8dd5e51e
ENNAMRI <aub.enmer@gmail.com>
2023-06-27 11:06:34 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,20 +1,16 @@
 #include "main.h"
+
 /**
-*_puts - prints string followed by newline.
-*@str: pointer to string.
-*
-*Return: void.
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
 */
 void _puts(char *str)
 {
 	int i;
 
-	i = 0;
-	while (*str != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(*str);
-		i++;
-		str++;
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
